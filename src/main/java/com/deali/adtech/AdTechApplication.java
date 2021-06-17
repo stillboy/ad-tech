eb149d67d8155ee73675271230ccaff64b753ad6
snlcom <admin@adminui-MacBookPro.local>
2021-06-17 17:39:44 +0900

    Initial commit

---
+++ src/main/java/com/deali/adtech/AdTechApplication.java
@@ -0,0 +1,13 @@
+package com.deali.adtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdTechApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdTechApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deali/adtech/AdTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deali.adtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
