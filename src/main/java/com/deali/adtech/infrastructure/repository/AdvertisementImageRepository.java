8c049ba58a36e1050a0ae739841943d2cacf9691
snlcom <admin@adminui-MacBookPro.local>
2021-06-18 10:40:11 +0900

    각 엔티티의 JPA 레포지토리 추가

---
+++ src/main/java/com/deali/adtech/infrastructure/repository/AdvertisementImageRepository.java
@@ -0,0 +1,7 @@
+package com.deali.adtech.infrastructure.repository;
+
+import com.deali.adtech.domain.AdvertisementImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdvertisementImageRepository extends JpaRepository<AdvertisementImage, Long> {
+}
---
+++ src/main/java/com/deali/adtech/infrastructure/repository/ExposeCountRepository.java
@@ -0,0 +1,7 @@
+package com.deali.adtech.infrastructure.repository;
+
+import com.deali.adtech.domain.ExposeCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExposeCountRepository extends JpaRepository<ExposeCount, Long> {
+}
